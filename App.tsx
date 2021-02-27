aba34d496798b7fe6731fe3a3befa8e12879e0c9
Matti Tuomikoski <mtuomikoski@gmail.com>
2021-02-27 15:50:54 +0200

    Change to TypeScript and add linting

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Lulz!</Text>
+      <Text>Open up App.js to start working on your app! Foo!</Text>
       <StatusBar style="auto" />
     </View>
   );
